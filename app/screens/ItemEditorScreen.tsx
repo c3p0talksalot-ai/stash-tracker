865676941203209c9642f7569bc899517abe3055
c3p0 talksalot <c3p0talksalot@gmail.com>
2026-03-29 14:50:28 -0600

    debug: Log in DB service

--- app/screens/ItemEditorScreen.tsx
+++ app/screens/ItemEditorScreen.tsx
@@ -202,7 +202,7 @@ export function ItemEditorScreen({ navigation, route }: Props) {
     setIsSaving(true)
     try {
       if (isEditing && itemId) {
-        console.log("updateItem called with", { name }); await updateItem(itemId, {
+        Alert.alert("DEBUG", "Saving name: " + name); console.log("updateItem called with", { name }); await updateItem(itemId, {
           name: name,
           description: description,
           location: location,
